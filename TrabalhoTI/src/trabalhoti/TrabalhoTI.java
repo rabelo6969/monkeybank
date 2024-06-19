6050b3d4bb04926ef10bd8fd16e192286a0f2434
rabelo6969 <jprabelo41@gmail.com>
2024-06-19 14:42:30 -0300

    Create TrabalhoTI.java

---
+++ TrabalhoTI/src/trabalhoti/TrabalhoTI.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package trabalhoti;
+
+import login.NewJFrame;
+
+/**
+ *
+ * @author Usuário
+ */
+public class TrabalhoTI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewJFrame frame = new NewJFrame();
+    }
+    
+}
